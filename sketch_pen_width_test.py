4ea5e7580a06d252578797f81327871bcac06f18
R4chel <rae84@cornell.edu>
2023-02-08 13:11:56 -0500

    change buffer calculation

--- sketch_pen_width_test.py
+++ sketch_pen_width_test.py
@@ -12,7 +12,7 @@ class PenWidthTestSketch(vsketch.SketchClass):
     min_pen_width = vsketch.Param(0.2, decimals=3, unit="mm")
     max_pen_width = vsketch.Param(1.0, decimals=3, unit="mm")
     num_steps = vsketch.Param(5)
-    buffer_ratio = vsketch.Param(0.1, decimals=4)
+    min_buffer = vsketch.Param(0.1, decimals=4, unit="in")
     label = vsketch.Param(False)
     label_height = vsketch.Param(12, unit="px")
     num_layers = vsketch.Param(2)
@@ -23,12 +23,11 @@ class PenWidthTestSketch(vsketch.SketchClass):
                  landscape=self.landscape,
                  center=False)
 
-        x_max_radius = vsk.width / (2 * self.num_steps + (self.num_steps + 1) *
-                                    (1 + self.buffer_ratio))
-        y_max_radius = (vsk.height -
-                        (self.label_height if self.label else 0)) / (
-                            2 * self.num_layers + (self.num_layers + 1) *
-                            (1 + self.buffer_ratio))
+        x_max_radius = (vsk.width - self.min_buffer *
+                        (self.num_steps + 1)) / (2 * self.num_steps)
+        y_max_radius = (vsk.height - (self.label_height if self.label else 0) -
+                        self.min_buffer *
+                        (self.num_layers + 1)) / (2 * self.num_layers)
 
         radius = min(x_max_radius, y_max_radius)
         x_buffer = (vsk.width -
